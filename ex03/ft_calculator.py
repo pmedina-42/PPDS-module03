b2096963410fda8c72c416e4a537bb2776ef346b
yop <patriciameedina@gmail.com>
2024-03-09 20:39:41 +0100

    ayayayyyyyyyyy

--- ex03/ft_calculator.py
+++ ex03/ft_calculator.py
@@ -7,24 +7,24 @@ class calculator:
     def __add__(self, object) -> None:
         """suma tu madre + puta"""
         try:
-            result = [val + object for val in self.vector]
-            print(result)
+            self.vector = [val + object for val in self.vector]
+            print(self.vector)
         except Exception:
             print('Bro?')
 
     def __mul__(self, object) -> None:
         """multiplica tu madre x guarra"""
         try:
-            result = [val * object for val in self.vector]
-            print(result)
+            self.vector = [val * object for val in self.vector]
+            print(self.vector)
         except Exception:
             print('Bro?')
 
     def __sub__(self, object) -> None:
         """resta tu madre - dignidad = tu madre también porque no tiene"""
         try:
-            result = [val - object for val in self.vector]
-            print(result)
+            self.vector = [val - object for val in self.vector]
+            print(self.vector)
         except Exception:
             print('Bro?')
 
@@ -33,8 +33,8 @@ class calculator:
         try:
             if object == 0:
                 raise ValueError("Division by zero is not allowed")
-            result = [val / object for val in self.vector]
-            print(result)
+            self.vector = [val / object for val in self.vector]
+            print(self.vector)
         except ValueError as ve:
             print(f'{type(ve).__name__}: {ve}')
         except Exception:
